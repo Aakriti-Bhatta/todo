dc16fd084d4b18697689280bb286bb86d07d38f2
Aakriti-bhatta <bhatta.aakriti81@gmail.com>
2020-11-25 17:33:50 +0545

    event use in input field

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
-import React from "react"
+import React, { useState } from "react"
  const App = ()=>{
+
+  const[inputList , setInputList] = useState("");
+
+  const itemsEvent =(event)=>{
+    setInputList(event.target.value)
+  }
    return(
      <>
      <div className="main_div">
@@ -7,8 +13,11 @@ import React from "react"
          <br/>
          <h1>ToDo List</h1>
          <br/>
-         <input type="text" placeholder="Add a items"/>
+         <input type="text" placeholder="Add a items" onChange={itemsEvent}/>
          <button>+</button>
+         <ol>
+           <li> {inputList} </li>
+         </ol>
        </div>
      </div>
      </>
