fa07c764f264e70d54aa7cc61525e163d727e677
Aakriti-bhatta <bhatta.aakriti81@gmail.com>
2020-11-26 10:16:01 +0545

    ToDOLists component created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React, { useState } from "react"
+import ToDoLists from "./ToDoLists";
  const App = ()=>{
 
   const[inputList , setInputList] = useState("");
+  const [Items , setItems] = useState([]);
 
   const itemsEvent =(event)=>{
     setInputList(event.target.value)
+  }
+
+  const handleClick = () => {
+    setItems( (oldItems) =>{
+      return [...oldItems , inputList];
+    });
+    setInputList('');
+
   }
    return(
      <>
@@ -13,10 +23,13 @@ import React, { useState } from "react"
          <br/>
          <h1>ToDo List</h1>
          <br/>
-         <input type="text" placeholder="Add a items" onChange={itemsEvent}/>
-         <button>+</button>
+         <input type="text" placeholder="Add a items" value={inputList} onChange={itemsEvent}/>
+         <button onClick = {handleClick}>+</button>
          <ol>
-           <li> {inputList} </li>
+           {/* <li> {inputList} </li> */}
+           {Items.map( (itemsval)=> {
+             <ToDoLists/>
+           })}
          </ol>
        </div>
      </div>
---
+++ src/ToDoLists.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ToDoLists = () => {
+return <li> { itemval } </li>
+};
+
+export default ToDoLists
